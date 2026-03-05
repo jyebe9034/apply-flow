dc758508a521943435f826afb7eb884d9b899b3b
hannah <jyebe9034@gmail.com>
2026-03-05 15:45:02 +0900

    feat: set up PostgreSQL with Docker and configure JPA database connection

--- backend/applyflow/src/main/java/com/hannah/applyflow/ApplyflowApplication.java
+++ backend/applyflow/src/main/java/com/hannah/applyflow/ApplyflowApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing 활성화라는데.. TODO: 여기에도 붙는게 맞는건지 확인 필요
 @SpringBootApplication
 public class ApplyflowApplication {
 
--- backend/applyflow/src/main/java/com/hannah/applyflow/job/repository/JobRepositoryImpl.java
+++ backend/applyflow/src/main/java/com/hannah/applyflow/job/repository/JobRepositoryImpl.java
@@ -1,10 +1,20 @@
 package com.hannah.applyflow.job.repository;
 
+import com.hannah.applyflow.job.Job;
+import com.hannah.applyflow.job.dto.JobSearchCondition;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class JobRepositoryImpl implements JobRepositoryCustom {
-    // QueryDsl 추가
+public class JobRepositoryImpl implements JobRepositoryCustom { // QueryDsl 추가
+
+    @Override
+    public Page<Job> search(JobSearchCondition condition, Pageable pageable) {
+        return null;
+    }
+
+
 }
