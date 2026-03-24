973d6dbee98ac2614a84b03c558bed82d7cca407
hannah <jyebe9034@gmail.com>
2026-03-24 13:51:32 +0900

    refactor: remove unused search method and JobSearchCondition

--- backend/applyflow/src/main/java/com/hannah/applyflow/job/repository/JobRepositoryCustom.java
+++ backend/applyflow/src/main/java/com/hannah/applyflow/job/repository/JobRepositoryCustom.java
@@ -1,7 +1,6 @@
 package com.hannah.applyflow.job.repository;
 
 import com.hannah.applyflow.job.Job;
-import com.hannah.applyflow.job.dto.JobSearchCondition;
 import com.hannah.applyflow.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +12,4 @@ public interface JobRepositoryCustom {
     List<Job> findAllByUser(User user);
 
     Page<Job> findAllByUser(User user, Pageable pageable);
-
-    Page<Job> search(JobSearchCondition condition, Pageable pageable);
 }
